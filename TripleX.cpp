a3cb3d8104dcb5c9cde55d3db1ee37b60526aa2c
(Charles) Zixin Zhang <zixinzha@seas.upenn.edu>
2021-10-13 12:14:33 -0400

    20. Statements & Comments

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,9 +2,11 @@
 
 int main()
 {
+    // print welcome messages to the terminal
     std::cout << "You are a secret agent breaking into a secure server room" << std::endl;;
     std::cout << "You need to enter the correct codes to continue..." << std::endl;
 
+    // declare 3 number code
     const int a = 4;
     const int b = 3;
     const int c = 2;
@@ -12,6 +14,7 @@ int main()
     const int sum = a + b + c;
     const int product = a * b * c;
 
+    // print sum and product to the terminal
     std::cout << sum << std::endl;
     std::cout << product << std::endl;
 
