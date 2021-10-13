ad144ffc834bf0fa061aff7df7d00edb7dc06f9e
(Charles) Zixin Zhang <zixinzha@seas.upenn.edu>
2021-10-13 12:10:18 -0400

    19. const & Assigning Values

--- TripleX.cpp
+++ TripleX.cpp
@@ -5,13 +5,12 @@ int main()
     std::cout << "You are a secret agent breaking into a secure server room" << std::endl;;
     std::cout << "You need to enter the correct codes to continue..." << std::endl;
 
-    
-    int a = 4;
-    int b = 3;
-    int c = 2;
+    const int a = 4;
+    const int b = 3;
+    const int c = 2;
 
-    int sum = a * b * c;
-    int product = a * b * c;
+    const int sum = a + b + c;
+    const int product = a * b * c;
 
     std::cout << sum << std::endl;
     std::cout << product << std::endl;
