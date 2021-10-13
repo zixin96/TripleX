76976f7f9bb01bb0c5e26c1bad625eb3812ed70c
(Charles) Zixin Zhang <zixinzha@seas.upenn.edu>
2021-10-13 11:54:09 -0400

    Hello World

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,4 +1,9 @@
+#include <iostream>
+
 int main()
 {
+    std::cout << "You are a secret agent breaking into a secure server room";
+    std::cout << std::endl;
+    std::cout << "You need to enter the correct codes to continue...";
     return 0;
 }
